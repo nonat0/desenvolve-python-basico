d1ac438820f87138402e9c7470292583536aeada
nonat0 <155467247+nonat0@users.noreply.github.com>
2025-04-07 03:48:13 -0300

    Delete Módulo 6 directory
    Deletei esta pasta pois possui o nome-seção igual as demais pastas porém contem acento agudo na letra o da palavra Módulo, o que deixa esta pasta desordenada da ordem original das pastas. Esta pasta já está de volta ao mesmo diretório, porém sem o acento para melhor organização.

--- Módulo 6/Aula 6.4 - Compreensão - Exercícios/aula6.4_questao2.py
+++
@@ -1,8 +0,0 @@
-frase = input("Digite uma frase: ")
-
-lista_vogais = "aeiouAEIOU"
-vogais = sorted([l for l in frase if l in lista_vogais])
-consoantes = [l for l in frase if l not in lista_vogais and l !=" "]
-
-print("\nVogais ordenadas: ", vogais)
-print("Consoantes: ", consoantes)
--- Módulo 6/Aula 6.4 - Compreensão - Exercícios/aula6.4_questao4.py
+++
@@ -1,4 +0,0 @@
-alunos = ["Maria", "jose", "Carla", "Sol"]
-notas = [35, 50, 20, 80]
-aprovados = [alunos[i] for i in range (len(notas)) if notas[i] >= 60 ]
-print(aprovados)
