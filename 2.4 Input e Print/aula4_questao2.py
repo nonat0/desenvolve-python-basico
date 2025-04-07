728179b18197d134d95f7ea4721057f4218b5c61
nonat0 <155467247+nonat0@users.noreply.github.com>
2025-04-07 03:43:26 -0300

    Delete 2.4 Input e Print directory
    Deletando as subpastas e reupando o conjunto de subpastas dentro de uma pasta principal para manter a página inicial do github mais organizada.

--- 2.4 Input e Print/aula4_questao2.py
+++
@@ -1,4 +0,0 @@
-farenheit = int(input("Insira a temperatura em Farenheit: "))
-celcius = (farenheit - 32) * (5/9)
-celcius = int(celcius)
-print(f"{farenheit} graus em farenheit são {celcius} graus celcius.")
